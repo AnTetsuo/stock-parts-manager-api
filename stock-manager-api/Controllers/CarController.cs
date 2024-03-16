e0ab4062f94da21edc5f5b711a1ffeed956efa20
Andrey Tetsuo <tetsuo_miyasato@hotmail.com>
2024-03-16 17:22:59 -0300

    feat(cars): add car controller

---
+++ stock-manager-api/Controllers/CarController.cs
@@ -0,0 +1,100 @@
+using Microsoft.AspNetCore.Mvc;
+using stock_manager_api.Dto;
+using stock_manager_api.Models;
+using stock_manager_api.Repository;
+
+namespace stock_manager_api.Controllers
+{
+    [ApiController]
+    [Route("cars")]
+    public class CarController: Controller 
+    {
+        private readonly CarRepository carRepository;
+        public CarController(CarRepository repository) { carRepository = repository; }
+
+        [HttpGet(Name = "GetCars")]
+        public IActionResult GetCars() 
+        {
+            try 
+            {
+                return Ok(carRepository.GetAll());        
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, new { message = "Internal Server Error"});
+            }
+        }
+
+        [HttpGet("{carId}")]
+        public IActionResult GetCarById(int carId)
+        {
+            try
+            {
+                ResponseCarDto carById = carRepository.GetById(carId);
+                
+                return Ok(carById);
+            } 
+            catch (KeyNotFoundException NotFoundEx)
+            {
+                return NotFound(NotFoundEx.Message);
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, new { message = "Internal Server Error" });
+            }
+        }
+
+        [HttpPost]
+        public IActionResult PostCar([FromBody] InsertCarDto car)
+        {
+            try
+            {
+                ResponseCarDto createdCar = carRepository.Add(car);
+
+                return CreatedAtRoute("GetCars", createdCar);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = ex.ToString() });
+            }
+        }
+
+        [HttpPut("{carId}")]
+        public IActionResult PutCar(int carId, [FromBody] InsertCarDto car)
+        {
+            try
+            {
+                ResponseCarDto carToEdit = carRepository.Update(car, carId);
+
+                return CreatedAtRoute("GetCars", carToEdit);
+            }
+            catch(KeyNotFoundException NotFoundEx)
+            {
+                return NotFound(NotFoundEx.Message);
+            }
+            catch(Exception)
+            {
+                return StatusCode(500, new { message = "Internal Server Error" });
+            }
+        }
+
+        [HttpDelete("{carId}")]
+        public IActionResult DeleteCar(int carId)
+        {
+            try
+            {
+                carRepository.Delete(carId);
+
+                return NoContent();
+            }
+            catch (KeyNotFoundException NotFoundEx)
+            {
+                return NotFound(NotFoundEx.Message);
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, new { message = "Internal Server Error" });
+            }
+        }
+    }
+}
